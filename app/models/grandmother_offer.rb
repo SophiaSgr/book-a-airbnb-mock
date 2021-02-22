205db7a692eea9373a04634e97c10458a14dd15e
Daniel Frieske <frieskedaniel@gmail.com>
2021-02-22 17:22:28 +0100

    changed columns in grandmotherOffer to one space back

--- app/models/grandmother_offer.rb
+++ app/models/grandmother_offer.rb
@@ -1,8 +1,8 @@
 class GrandmotherOffer < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
-  validates :first_name, presence :true
-  validates :last_name, presence :true
-  validates :username, presence :true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
   validates :description, length: { minimum: 30 }
 end
