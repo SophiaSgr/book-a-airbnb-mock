03cf9649b566002c8fb1649bb104ed4fc3dccb46
Daniel Frieske <frieskedaniel@gmail.com>
2021-02-22 16:17:41 +0100

    created grandmother_offer model and did references + validations

---
+++ db/migrate/20210222150112_create_grandmother_offers.rb
@@ -0,0 +1,14 @@
+class CreateGrandmotherOffers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :grandmother_offers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.integer :price_per_hour
+      t.string :activity_type
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
