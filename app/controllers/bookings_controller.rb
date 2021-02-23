04c1945aaa9df13f171ec7dd28cb1ee0cadbcaa0
Sophia Sager <sophia.sager@outlook.de>
2021-02-23 15:13:33 +0100

    added bookings controller and views except for show view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,27 +5,29 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id]
+    @booking = Booking.find(params[:id])
   end
 
   def new
-    Booking.new
+    @booking = Booking.new
+    @grandmother_offer = GrandmotherOffer.find(params[:grandmother_offer_id])
   end
 
   def create
     @grandmother_offer = GrandmotherOffer.find(params[:grandmother_offer_id])
     @booking = Booking.new(booking_params)
-    @booking.grandmother_offers = @grandmother_offer
+    @booking.user = current_user
+    @booking.grandmother_offer = @grandmother_offer
     if @booking.save
-      redirect_to  grandmother_offer_booking_path(@grandmother_offer)# ROUTE
+      redirect_to grandmother_offer_path(@grandmother_offer)
     else
-      render :new # NOT A ROUTE, IT'S A FILEPATH FROM /views
+      render :new
     end
   end
 
   private
 
   def booking_params
-    params.require(:bookings).permit(:booking_date, :status)
+    params.require(:booking).permit(:booking_date, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_02_23_100627) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "bookings", force: :cascade do |t|
     t.string "status", default: "pending"
