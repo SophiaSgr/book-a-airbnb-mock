d005d07a5fdb73b055538ad2472708b070082e6f
Daniel Frieske <frieskedaniel@gmail.com>
2021-02-25 11:56:39 +0100

    added the gemfile geocoder and added column latitude and longitude to grandmother offers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_103940) do
+ActiveRecord::Schema.define(version: 2021_02_25_105324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_02_24_103940) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_grandmother_offers_on_user_id"
   end
 
